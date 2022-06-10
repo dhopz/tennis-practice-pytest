31cc2a74d77c4569ee29003f876d53c8677a81b8
dhopz <david_houpapa@hotmail.com>
2022-06-10 15:10:42 +0100

    figuring our asserts

--- tennis_scoring/scoring.py
+++ tennis_scoring/scoring.py
@@ -7,3 +7,19 @@ class Score():
 def is_even(n): 
     # your code here
     return n%2 == 0
+
+def paul(x):
+    points = {'kata':5, "Petes kata":10, "life":0, "eating":1}
+    score = 0
+
+    for i in x:
+        score += points[i]
+
+    if score < 40:
+        return "Super happy!"
+    elif score < 70:
+        return "Happy!"
+    elif score < 100:
+        return "Sad!"
+    else:
+        return "Miserable!"
--- tests/test_scoring.py
+++ tests/test_scoring.py
@@ -1,8 +1,13 @@
 import pytest
 import json
 import datetime
-from tennis_scoring.scoring import is_even
+from tennis_scoring.scoring import is_even,paul
 
 def test_is_even():
     assert is_even(2) == True
+
+def test_paul():
+    assert paul(['life', 'eating', 'life']) == 'Super happy!'
+    assert paul(['life', 'Petes kata', 'Petes kata', 'Petes kata', 'eating']) == 'Super happy!'
+    assert paul(['Petes kata', 'Petes kata', 'eating', 'Petes kata', 'Petes kata', 'eating']) == 'Happy!'
 
